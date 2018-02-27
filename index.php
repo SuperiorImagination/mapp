dcc024e851b34d79b749b2d9f4c83c9f08b0513a
callumchurch8 <33250565+callumchurch8@users.noreply.github.com>
2018-02-27 13:17:13 +0000

    index.php
    Home Page

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width"/>
+    <link rel="stylesheet" type="text/css" href="stylesheet.css" />
+    <link href='http://fonts.googleapis.com/css?family=Lato&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+    <title>Mapp - Cloud Workspace</title>
+</head>
+
+<body>
+
+<nav>
+    <ul>
+        <li><img src="logo-nav.png" class="logo-nav"></li>
+        <li><a href="">About</a></li>
+        <li><a href="">Store</a></li>
+        <li><a href="">Support</a></li>
+        <li class="right"><a href="">Cart</a></li>
+        <li class="right"><a href="">Log In</a></li>
+
+    </ul>
+</nav>
+
+</body>
+</html>
